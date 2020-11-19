9c310ee2c2fab3c859a250529d5b7994ac5c176d
Tahiana <danielson@hairun-technology.com>
2020-11-19 22:51:46 +0300

    init redux

--- src/components/HashSearchBar/HashSearchbar.jsx
+++ src/components/HashSearchBar/HashSearchbar.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { connect } from "react-redux";
+import { changeSearch } from "../../store/actions/search";
 import "./hashsearchbar.scss";
 
 function HashSearchBar(props) {
-  const { handleSubmit } = props;
-  const [ input, setInput ] = useState("");
+  const { handleSubmit, inputVal, changeSearch } = props;
+  // const [ input, setInput ] = useState("");
 
-  const inputChange = (e) => {
-    setInput(e.target.value);
-  };
+  // const inputChange = (e) => {
+  //   setInput(e.target.value);
+  // };
 
   return (
     <form className="input-group" onSubmit={handleSubmit}>
-      <input type="text" onChange={inputChange} value={input} className="form-control" placeholder="#search" />
+      <input type="text" onChange={changeSearch} value={inputVal} className="form-control" placeholder="#search" />
       <div className="input-group-append">
         <FaSearch />
       </div>
@@ -20,4 +22,19 @@ function HashSearchBar(props) {
   );
 }
 
-export default HashSearchBar;
+const mapStateToProps = (state) => {
+  console.log("here is the state", state);
+  return {
+    inputVal: state.search.inputValue
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeSearch: (evt) => {
+      dispatch(changeSearch(evt));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HashSearchBar);
---
+++ src/store/actions/search.js
@@ -0,0 +1,6 @@
+import { ERROR_SEARCH, SUBMIT_SEARCH, CHANGE_SEARH, NEW_SEARCH_DATA } from "../action-types";
+
+export const changeSearch = (evt) => ({
+  type: CHANGE_SEARH,
+  payload: { value: evt.target.value }
+});
